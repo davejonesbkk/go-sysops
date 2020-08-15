0a8f87afef5eae0cd72ab1948dc551e45c1d7e67
David Jones <davejonesbkk@gmail.com>
2020-08-15 10:24:25 +0100

    added flag for passing directory

--- sysops/listdir.go
+++ sysops/listdir.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"flag"
 )
 
 func listdir() {
-	file, err := ioutil.ReadDir("./")
+
+	dirpath := flag.String("directory", "/foo", "The directory we want to list files in")
+
+	flag.Parse()
+
+	fmt.Println("directory:", *dirpath)
+
+	file, err := ioutil.ReadDir(*dirpath)
 	if err != nil {
 		log.Fatal(err)
 	}
